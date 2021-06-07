7a2ffe2db48ce04bae29dae8bc5d7ae8c26af309
PratikGarai <pratikgarai0208@gmail.com>
2021-06-08 00:55:26 +0530

    Report Addition success!

--- sales/sale/static/sale/home.js
+++ sales/sale/static/sale/home.js
@@ -2,7 +2,42 @@ console.log("Home Script Loaded!");
 
 const reportBtn = document.getElementById("report-btn");
 const img = document.getElementById("img");
+const modalBody = document.getElementById("modal-body");
+const reportForm = document.getElementById("report-form");
+
+const reportName = document.getElementById('id_name');
+const reportRemarks = document.getElementById('id_remarks');
+const csrf = document.getElementsByName("csrfmiddlewaretoken")[1].value;
 
 if(img) {
     reportBtn.classList.remove('not-visible');
 }
+
+reportBtn.addEventListener('click', () => {
+    const im2 = img;
+    im2.setAttribute('class', 'w-100');
+    modalBody.prepend(im2);
+    
+    reportForm.addEventListener('submit', e=> {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("csrfmiddlewaretoken", csrf);
+        formData.append("name", reportName.value);
+        formData.append("remarks", reportRemarks.value);
+        formData.append("image", img.src);
+
+        $.ajax({
+            type : "POST",
+            url : '/reports/save/',
+            data : formData,
+            success : function(response) {
+                console.log("Success : ", response)
+            },
+            error : function(response) {
+                console.log("Error : ", response)
+            },
+            processData : false, 
+            contentType : false
+        })
+    })
+});
--- sales/sales/urls.py
+++ sales/sales/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sale.urls'), name='sale'),
+    path('reports/', include('report.urls'), name='report')
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
