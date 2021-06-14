5298f0c162e3bc96a14404fa6c7ca9eceacba0db
PratikGarai <pratikgarai0208@gmail.com>
2021-06-14 07:37:16 +0530

    Profile update view

--- sales/sales/urls.py
+++ sales/sales/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sale.urls'), name='sale'),
-    path('reports/', include('report.urls'), name='report')
+    path('reports/', include('report.urls'), name='report'),
+    path('profile/', include('profiles.urls'), name='profiles')
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
