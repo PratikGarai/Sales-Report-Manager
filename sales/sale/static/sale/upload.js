fb6bcc5a6cc334ac43432d94cf05ac6659e87ac2
PratikGarai <pratikgarai0208@gmail.com>
2021-06-14 07:13:04 +0530

    Minor enhanceents in the uploading mechanism

--- sales/sale/static/sale/upload.js
+++ sales/sale/static/sale/upload.js
@@ -1,4 +1,5 @@
 const csrf = document.getElementsByName("csrfmiddlewaretoken")[0].value;
+const alertBox = document.getElementById("alert-box");
 
 Dropzone.autoDiscover = false;
 const myDropzone = new Dropzone("#my-dropzone", {
@@ -6,6 +7,31 @@ const myDropzone = new Dropzone("#my-dropzone", {
     init : function() {
         this.on('sending', function(file, xhr, formData) {
             formData.append("csrfmiddlewaretoken", csrf);
+        });
+
+        this.on('success', function(file, response) {
+            const ex = response.ex;
+            if(ex) {
+                alertBox.innerHTML = `
+                <div class="alert alert-danger" role="alert">
+                    Error uploading file!
+                </div>
+                `;
+            } else {
+                alertBox.innerHTML = `
+                <div class="alert alert-success" role="alert">
+                    File uploaded successfully!
+                </div>
+                `;
+            }
+        });
+
+        this.on('error', function(file, response) {
+            alertBox.innerHTML = `
+            <div class="alert alert-danger" role="alert">
+                Error uploading file!
+            </div>
+            `;
         })
     }, 
     maxFiles : 3, 
